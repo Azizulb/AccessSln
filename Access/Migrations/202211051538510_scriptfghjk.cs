899f3f6d598dd6b89909be72ed01e75e8f578abb
user <user@DESKTOP-49PGGOU>
2022-12-01 12:19:36 +0600

    Add project files.

---
+++ Access/Migrations/202211051538510_scriptfghjk.cs
@@ -0,0 +1,16 @@
+﻿namespace Access.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class scriptfghjk : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Access/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Access.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Access.Models.AccessDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Access.Models.AccessDBContext";
+        }
+
+        protected override void Seed(Access.Models.AccessDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
